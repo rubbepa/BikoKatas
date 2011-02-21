8b7dac1853692a5e5d7c827eae59ee30a79d4c67
Rubén Bernárdez <ruben.bernardez@biko2.com>
2011-02-21 14:38:17 +0100

    Kata realizada en biko por el equipo en 1h 40m

---
+++ StringCalculator/equipo/StringCalculator.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorLive
+{
+    public class StringCalculator
+    {
+        private const char INICIO_DELIMITADOR_COMPLEJO = '[';
+        private const char FIN_DELIMITADOR_COMPLEJO = ']';
+        private const string INICIO_ZONA_DELIMITADORES = "//";
+        private const string FIN_ZONA_DELIMITADORES = "\n";
+
+        public int Add(string sumandos)
+        {
+            if (string.IsNullOrEmpty(sumandos))
+            {
+                return 0;
+            }
+
+            List<int> listaNumeros = ConvertirCadenaExtraida(sumandos);
+
+            ComprobarQueNoExistenNumerosNegativos(listaNumeros);
+
+            listaNumeros = EliminarNumerosMayoresQue1000(listaNumeros);
+
+            return SumaDeSumandos(listaNumeros);
+        }
+
+        private List<int> EliminarNumerosMayoresQue1000(List<int> listaNumeros)
+        {
+            listaNumeros = listaNumeros.Where(x => x <= 1000).ToList();
+            return listaNumeros;
+        }
+
+        private void ComprobarQueNoExistenNumerosNegativos(List<int> listaNumeros)
+        {
+            
+            List<int> numerosNegativos = listaNumeros.Where(x => x < 0).ToList();
+
+           
+            if (numerosNegativos.Count > 0)
+            {
+                throw new Exception(
+                    "negatives not allowed: " + string.Join(",", numerosNegativos));
+            }
+        }
+
+        private int SumaDeSumandos(List<int> listaNumeros)
+        {
+            int resultado = 0;
+            foreach (int numero in listaNumeros)
+            {
+                    resultado = resultado + numero;
+            }
+            return resultado;
+        }
+
+        private List<int> ConvertirCadenaExtraida(string cadenaEnBruto)
+        {
+            List<string> delimitadores = new List<string>() {",", "\n"};
+
+            if(ExisteDelimitadorPersonalizado(cadenaEnBruto))
+            {
+                delimitadores.Add(ObtenerDelimitadorPersonalizado(cadenaEnBruto));
+                cadenaEnBruto = EliminarSeccionDeDelimitadores(cadenaEnBruto);
+            }
+
+            string[] numerosEnBruto = 
+                cadenaEnBruto.Split(delimitadores.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            List<int> resultados = new List<int>();
+
+            foreach (string numero in numerosEnBruto)
+            {
+                resultados.Add(int.Parse(numero));    
+            }
+
+            return resultados;
+        }
+
+        private string EliminarSeccionDeDelimitadores(string cadenaEnBruto)
+        {
+            cadenaEnBruto = cadenaEnBruto.Substring(cadenaEnBruto.IndexOf(FIN_ZONA_DELIMITADORES) + 1);
+            return cadenaEnBruto;
+        }
+
+        private string ObtenerDelimitadorPersonalizado(string cadenaEnBruto)
+        {
+            if (ExisteDelimitadorPersonalizadoComplejo(cadenaEnBruto))
+            {
+                return ObtenerDelimitadoresPersonalizadosComplejos(cadenaEnBruto);    
+            }
+            return ObtenerDelimitadorSimple(cadenaEnBruto);
+
+        }
+
+        private bool ExisteDelimitadorPersonalizadoComplejo(string cadenaEnBruto)
+        {
+            return cadenaEnBruto.StartsWith(INICIO_ZONA_DELIMITADORES + INICIO_DELIMITADOR_COMPLEJO);
+        }
+
+        private string ObtenerDelimitadoresPersonalizadosComplejos(string cadenaEnBruto)
+        {
+            int inicioDelimitadorComplejo = cadenaEnBruto.IndexOf(INICIO_DELIMITADOR_COMPLEJO) + 1;
+            int finDelimitadorComplejo = cadenaEnBruto.IndexOf(FIN_DELIMITADOR_COMPLEJO);
+
+            return cadenaEnBruto.Substring(
+                inicioDelimitadorComplejo, 
+                finDelimitadorComplejo - inicioDelimitadorComplejo);
+        }
+
+        private string ObtenerDelimitadorSimple(string cadenaEnBruto)
+        {
+            return cadenaEnBruto[2].ToString();
+        }
+
+        private bool ExisteDelimitadorPersonalizado(string sumandos)
+        {
+            return sumandos.StartsWith(INICIO_ZONA_DELIMITADORES);
+        }
+    }
+}
---
+++ StringCalculator/equipo/StringCalculatorTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace StringCalculatorLive
+{
+    public class StringCalculatorTests
+    {
+        private StringCalculator _stringCalculator;
+        private int _resultado = 0;
+
+        public StringCalculatorTests()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void LaSumaSinSumandosEsCero()
+        {
+            _resultado = _stringCalculator.Add("");
+
+            Assert.Equal(_resultado, 0);
+        }
+
+        [Fact]
+        public void LaSumaConUnSoloSumandoEsElSumando()
+        {
+            _resultado = _stringCalculator.Add("1");
+
+            Assert.Equal(_resultado, 1);
+        }
+
+        [Fact]
+        public void LaSumaConDosSumandoEsLaSumaDeEllos()
+        {
+            _resultado = _stringCalculator.Add("1,2");
+
+            Assert.Equal(_resultado, 3);
+        }
+
+        [Fact]
+        public void Deberia_poder_sumar_mas_de_2_numeros()
+        {
+            _resultado = _stringCalculator.Add("1,2,3");
+
+            Assert.Equal(_resultado, 6);
+        }
+
+        [Fact]
+        public void Deberia_poder_separar_con_salto_de_linea()
+        {
+            _resultado = _stringCalculator.Add("1\n2,3");
+
+            Assert.Equal(_resultado,6);
+        }
+
+        [Fact]
+        public void SumarNumerosConDelimitadoresPersonalizados()
+        {
+            _resultado = _stringCalculator.Add("//;\n1;2");
+            Assert.Equal(_resultado,3);
+        }
+
+        [Fact]
+        public void Deberia_no_soportar_un_numero_negativos()
+        {
+            Exception excepcion = Assert.Throws<Exception>(() => _stringCalculator.Add("-1,2"));
+            Assert.Contains("negatives not allowed", excepcion.Message);
+            Assert.Contains("-1", excepcion.Message);
+        }
+
+        [Fact]
+        public void Deberia_no_soportar_varios_numeros_negativos()
+        {
+            Exception excepcion = Assert.Throws<Exception>(() => _stringCalculator.Add("-1,-2"));
+            Assert.Contains("negatives not allowed", excepcion.Message);
+            Assert.Contains("-1", excepcion.Message);
+            Assert.Contains("-2", excepcion.Message);
+        }
+
+        [Fact]
+        public void Los_numeros_mayores_que_1000_son_ignorados()
+        {
+            _resultado = _stringCalculator.Add("1001,2");
+            Assert.Equal(_resultado,2);
+        }
+
+        [Fact]
+        public void El_delimitador_puede_tener_varios_caracteres()
+        {
+            _resultado = _stringCalculator.Add("//[***]\n1***2***3");
+            Assert.Equal(_resultado,6);
+        }
+
+        [Fact]
+        public void Deberia_devolver_cero_con_nulo()
+        {
+            Assert.Equal(0, _stringCalculator.Add(null));
+        }
+    }
+}
