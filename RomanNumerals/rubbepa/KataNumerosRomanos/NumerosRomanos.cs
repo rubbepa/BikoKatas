630e5ec8aab3425b5759e351edb0c04983d69d5f
Rubén Bernárdez <ruben.bernardez@biko2.com>
2011-03-07 00:06:23 +0100

    Roman Numerals de rubbepa - Primera aproximación

---
+++ RomanNumerals/rubbepa/KataNumerosRomanos/NumerosRomanos.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+namespace KataNumerosRomanos
+{
+    public class NumerosRomanos
+    {
+        private List<string> _unidades = 
+            new List<string>{"I","II","III","IV","V","VI","VII","VIII","IX"};
+        private List<string> _decenas =
+            new List<string> { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        private List<string> _centenas =
+            new List<string> { "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+
+        public string ConvertirNumeroEntero(int numeroAConvertir)
+        {
+            if (numeroAConvertir == 0) return "";
+
+            int millares = numeroAConvertir / 1000;
+            int centenas = numeroAConvertir / 100 - millares * 10;
+            int decenas = numeroAConvertir / 10 - millares * 100 - centenas * 10;
+            int unidades = numeroAConvertir - millares * 1000 - centenas * 100 - decenas * 10;
+            
+            string romanoResultado = string.Empty;
+            for (int x = 1; x <= millares; x++) romanoResultado += "M";
+            if (centenas > 0) romanoResultado += _centenas[centenas - 1];
+            if (decenas > 0) romanoResultado += _decenas[decenas - 1];
+            if (unidades > 0) romanoResultado += _unidades[unidades - 1];
+
+            return romanoResultado;
+        }
+
+        public int ConvertirRomano(string numeroRomano)
+        {
+            if (numeroRomano == string.Empty) return 0;
+
+            int pos = 0;
+            string millares = string.Empty;
+            while(pos < numeroRomano.Length && numeroRomano[pos] == 'M')
+            {
+                millares += numeroRomano[pos];
+                pos++;
+            }
+
+            string centenas = string.Empty;
+            while (pos < numeroRomano.Length && 
+                  (numeroRomano[pos] == 'C' || numeroRomano[pos] == 'D' || numeroRomano[pos] == 'M'))
+            {
+                centenas += numeroRomano[pos];
+                pos++;
+            }
+
+            string decenas = string.Empty;
+            while (pos < numeroRomano.Length &&
+                  (numeroRomano[pos] == 'X' || numeroRomano[pos] == 'L' || numeroRomano[pos] == 'C'))
+            {
+                decenas += numeroRomano[pos];
+                pos++;
+            }
+
+            string unidades = string.Empty;
+            while (pos < numeroRomano.Length)
+            {
+                unidades += numeroRomano[pos];
+                pos++;
+            }
+
+            int enteroResultado;
+
+            enteroResultado = _unidades.FindIndex(s => s == unidades) + 1;
+            enteroResultado += (_decenas.FindIndex(s => s == decenas) + 1) * 10;
+            enteroResultado += (_centenas.FindIndex(s => s == centenas) + 1) * 100;
+            enteroResultado += millares.Length * 1000;
+
+            return enteroResultado;
+        }
+    }
+}
---
+++ RomanNumerals/rubbepa/KataNumerosRomanos/NumerosRomanosTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace KataNumerosRomanos
+{
+    public class NumerosRomanosTest
+    {
+        private NumerosRomanos _numerosRomanos;
+
+        public NumerosRomanosTest()
+        {
+            _numerosRomanos = new NumerosRomanos();
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_unidades()
+        {
+            Assert.Equal("I", _numerosRomanos.ConvertirNumeroEntero(1));
+            Assert.Equal("II", _numerosRomanos.ConvertirNumeroEntero(2));
+            Assert.Equal("III", _numerosRomanos.ConvertirNumeroEntero(3));
+            Assert.Equal("IV", _numerosRomanos.ConvertirNumeroEntero(4));
+            Assert.Equal("V", _numerosRomanos.ConvertirNumeroEntero(5));
+            Assert.Equal("VI", _numerosRomanos.ConvertirNumeroEntero(6));
+            Assert.Equal("VII", _numerosRomanos.ConvertirNumeroEntero(7));
+            Assert.Equal("VIII", _numerosRomanos.ConvertirNumeroEntero(8));
+            Assert.Equal("IX", _numerosRomanos.ConvertirNumeroEntero(9));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_decenas()
+        {
+            Assert.Equal("X", _numerosRomanos.ConvertirNumeroEntero(10));
+            Assert.Equal("XX", _numerosRomanos.ConvertirNumeroEntero(20));
+            Assert.Equal("XXX", _numerosRomanos.ConvertirNumeroEntero(30));
+            Assert.Equal("XL", _numerosRomanos.ConvertirNumeroEntero(40));
+            Assert.Equal("L", _numerosRomanos.ConvertirNumeroEntero(50));
+            Assert.Equal("LX", _numerosRomanos.ConvertirNumeroEntero(60));
+            Assert.Equal("LXX", _numerosRomanos.ConvertirNumeroEntero(70));
+            Assert.Equal("LXXX", _numerosRomanos.ConvertirNumeroEntero(80));
+            Assert.Equal("XC", _numerosRomanos.ConvertirNumeroEntero(90));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_centenas()
+        {
+            Assert.Equal("C", _numerosRomanos.ConvertirNumeroEntero(100));
+            Assert.Equal("CC", _numerosRomanos.ConvertirNumeroEntero(200));
+            Assert.Equal("CCC", _numerosRomanos.ConvertirNumeroEntero(300));
+            Assert.Equal("CD", _numerosRomanos.ConvertirNumeroEntero(400));
+            Assert.Equal("D", _numerosRomanos.ConvertirNumeroEntero(500));
+            Assert.Equal("DC", _numerosRomanos.ConvertirNumeroEntero(600));
+            Assert.Equal("DCC", _numerosRomanos.ConvertirNumeroEntero(700));
+            Assert.Equal("DCCC", _numerosRomanos.ConvertirNumeroEntero(800));
+            Assert.Equal("CM", _numerosRomanos.ConvertirNumeroEntero(900));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_millares()
+        {
+            Assert.Equal("M", _numerosRomanos.ConvertirNumeroEntero(1000));
+            Assert.Equal("MM", _numerosRomanos.ConvertirNumeroEntero(2000));
+            Assert.Equal("MMM", _numerosRomanos.ConvertirNumeroEntero(3000));
+            Assert.Equal("MMMMMM", _numerosRomanos.ConvertirNumeroEntero(6000));
+        }
+
+        [Fact]
+        public void Deberia_convertir_numeros_complejos_de_dos_digitos()
+        {
+            Assert.Equal("XXV", _numerosRomanos.ConvertirNumeroEntero(25));
+            Assert.Equal("LXXXIX", _numerosRomanos.ConvertirNumeroEntero(89));
+        }
+
+        [Fact]
+        public void Deberia_convertir_numeros_complejos_de_varios_digitos()
+        {
+            Assert.Equal("CCCXLI", _numerosRomanos.ConvertirNumeroEntero(341));
+            Assert.Equal("DCCXXI", _numerosRomanos.ConvertirNumeroEntero(721));
+            Assert.Equal("CMXII", _numerosRomanos.ConvertirNumeroEntero(912));
+            Assert.Equal("DCCCXI", _numerosRomanos.ConvertirNumeroEntero(811));
+            Assert.Equal("MDCXXXII", _numerosRomanos.ConvertirNumeroEntero(1632));
+            Assert.Equal("MMC", _numerosRomanos.ConvertirNumeroEntero(2100));
+            Assert.Equal("MMMMCMXXX", _numerosRomanos.ConvertirNumeroEntero(4930));
+        }
+
+        [Fact]
+        public void Deberia_convertir_romano_a_entero()
+        {
+            Assert.Equal(20, _numerosRomanos.ConvertirRomano("XX"));
+            Assert.Equal(4930, _numerosRomanos.ConvertirRomano("MMMMCMXXX"));
+            Assert.Equal(811, _numerosRomanos.ConvertirRomano("DCCCXI"));
+            Assert.Equal(2000, _numerosRomanos.ConvertirRomano("MM"));
+            Assert.Equal(401, _numerosRomanos.ConvertirRomano("CDI"));
+            Assert.Equal(25, _numerosRomanos.ConvertirRomano("XXV"));
+            Assert.Equal(2, _numerosRomanos.ConvertirRomano("II"));
+        }
+
+        [Fact]
+        public void Deberia_devolver_cero_con_cadena_vacia()
+        {
+            Assert.Equal(0, _numerosRomanos.ConvertirRomano(""));
+        }
+
+        [Fact]
+        public void Deberia_convertir_cero_a_cadena_vacia()
+        {
+            Assert.Equal("", _numerosRomanos.ConvertirNumeroEntero(0));
+        }
+    }
+}
