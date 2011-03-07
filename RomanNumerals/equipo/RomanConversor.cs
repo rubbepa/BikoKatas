ebc1570f692d6434cad914f6dcdb39d28d2d7e71
Rubén Bernárdez <ruben.bernardez@biko2.com>
2011-03-07 10:06:23 +0100

    RomanNumerals hecha en equipo en 1:10

---
+++ RomanNumerals/equipo/RomanConversor.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanNumerals
+{
+    class RomanConversor
+    {
+        private Dictionary<int, string> _simbolosDeUnidades;
+        private Dictionary<int, string> _simbolosDeDecenas;
+        private Dictionary<int, string> _simbolosDeCentenas;
+
+        public RomanConversor()
+        {
+            InicializarUnidades();
+            InicializarDecenas();
+            InicializarCentenas();
+        }
+
+        private void InicializarUnidades()
+        {
+            _simbolosDeUnidades = new Dictionary<int, string>();
+
+            _simbolosDeUnidades.Add(0, "");
+            _simbolosDeUnidades.Add(1,"I");
+            _simbolosDeUnidades.Add(2, "II");
+            _simbolosDeUnidades.Add(3, "III");
+            _simbolosDeUnidades.Add(4, "IV");
+            _simbolosDeUnidades.Add(5, "V");
+            _simbolosDeUnidades.Add(6, "VI");
+            _simbolosDeUnidades.Add(7, "VII");
+            _simbolosDeUnidades.Add(8, "VIII");
+            _simbolosDeUnidades.Add(9, "IX");
+
+           
+        }
+
+        private void InicializarDecenas()
+        {
+            _simbolosDeDecenas = new Dictionary<int, string>();
+            _simbolosDeDecenas.Add(1,"X");
+            _simbolosDeDecenas.Add(2, "XX");
+            _simbolosDeDecenas.Add(3, "XXX");
+            _simbolosDeDecenas.Add(4, "XL");
+            _simbolosDeDecenas.Add(5, "L");
+            _simbolosDeDecenas.Add(6, "LX");
+            _simbolosDeDecenas.Add(7, "LXX");
+            _simbolosDeDecenas.Add(8, "LXXX");
+            _simbolosDeDecenas.Add(9, "XC");
+        }
+
+        private void InicializarCentenas()
+        {
+            _simbolosDeCentenas = new Dictionary<int, string>();
+            _simbolosDeCentenas.Add(1, "C");
+            _simbolosDeCentenas.Add(2, "CC");
+            _simbolosDeCentenas.Add(3, "CCC");
+            _simbolosDeCentenas.Add(4, "CD");
+            _simbolosDeCentenas.Add(5, "D");
+            _simbolosDeCentenas.Add(6, "DC");
+            _simbolosDeCentenas.Add(7, "DCC");
+            _simbolosDeCentenas.Add(8, "DCCC");
+            _simbolosDeCentenas.Add(9, "CM");
+        }
+
+        public string ConvertirARomanos(int numero)
+        {
+            string numeroRomano = string.Empty;
+
+            int centena = numero/100;
+            int decena = (numero%100)/10;
+            int unidad = numero - decena*10;
+
+            if (centena > 0)
+            {
+                numeroRomano += _simbolosDeCentenas[centena];
+            }
+            if (decena > 0)
+            {
+                numeroRomano += _simbolosDeDecenas[decena];
+            }
+
+            if (unidad > 0)
+            {
+                numeroRomano += _simbolosDeUnidades[unidad];
+            }
+
+            return numeroRomano;
+        }
+    }
+}
---
+++ RomanNumerals/equipo/RomanConversorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RomanNumerals
+{
+    public class RomanConversorTests
+    {
+        RomanConversor _romanConversor;
+        public RomanConversorTests()
+        {
+            _romanConversor = new RomanConversor();    
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_unidades()
+        {
+            Assert.Equal("I", _romanConversor.ConvertirARomanos(1));
+            Assert.Equal("V", _romanConversor.ConvertirARomanos(5));
+            Assert.Equal("VII", _romanConversor.ConvertirARomanos(7));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_decenas()
+        {
+            Assert.Equal("X",_romanConversor.ConvertirARomanos(10));
+            Assert.Equal("XX", _romanConversor.ConvertirARomanos(20));
+            Assert.Equal("XXX", _romanConversor.ConvertirARomanos(30));
+            Assert.Equal("XL", _romanConversor.ConvertirARomanos(40));
+            Assert.Equal("L", _romanConversor.ConvertirARomanos(50));
+            Assert.Equal("LX", _romanConversor.ConvertirARomanos(60));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_numeros_de_dos_digitos()
+        {
+            Assert.Equal("XII", _romanConversor.ConvertirARomanos(12));
+            Assert.Equal("LXXII", _romanConversor.ConvertirARomanos(72));
+            Assert.Equal("XCIX", _romanConversor.ConvertirARomanos(99));
+        }
+
+        [Fact]
+        public void Deberia_poder_convertir_centenas()
+        {
+            Assert.Equal("C",_romanConversor.ConvertirARomanos(100));
+        }
+    }
+}
